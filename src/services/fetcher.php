d6cc9eeceb3365a6a02f60e3230efe2d11f4c467
Ider <ider.cs@gmail.com>
2012-12-17 15:51:52 -0800

    base fetcher class

--- src/services/fetcher.php
+++ src/services/fetcher.php
@@ -1,10 +1,33 @@
 <?
+include_once './src/models/theater.php';
+include_once './src/models/movie.php';
 
-class TheatersFetcher {
+abstract class TheatersFetcher {
+    protected $theaterList;
 
+    public function __construct($zipcode) {
+        $this->theaterList = new TheaterList();
+        $this->theaterList->zipcode = $zipcode;
+    }
+
+    /**
+     * Fetch a list of theaters infomation associated with the zipcode
+     */
+    abstract public function fetchTheaters();
 }
 
 
-class MoviesFetcher {
-    
+abstract class MoviesFetcher {
+    protected $movieList;
+    public function __construct(Theater $theater, $date) {
+        $list = new MovieList();
+        $list->date = $date;
+        $list->theater = $theater;
+        $this->movieList = $list;
+    }
+
+    /**
+     * Fetch a list of movies infomation that showing in theater in that date
+     */
+    abstract public abstract fetchTheaterMovies();
 }
