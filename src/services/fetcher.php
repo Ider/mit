3b41833893762ebcac17abda74fd80c940366310
Ider <ider.cs@gmail.com>
2012-12-17 21:26:10 -0800

    implement google movies fetcher
    with regular expression and string split way, implement fetch movielist
    functionality

--- src/services/fetcher.php
+++ src/services/fetcher.php
@@ -1,33 +1,48 @@
-<?
-include_once './src/models/theater.php';
-include_once './src/models/movie.php';
+<?php
+include_once 'src/models/theater.php';
+include_once 'src/models/movie.php';
 
 abstract class TheatersFetcher {
     protected $theaterList;
 
-    public function __construct($zipcode) {
+    public function __construct($zipcode = '') {
         $this->theaterList = new TheaterList();
         $this->theaterList->zipcode = $zipcode;
+    }
+
+    public function theaterList() {
+        if (empty($this->theaterList->theaters)) {
+            $this->theaterList->theaters = $this->fetchTheaters();
+        }
+
+        return $this->theaterList;
     }
 
     /**
      * Fetch a list of theaters infomation associated with the zipcode
      */
-    abstract public function fetchTheaters();
+    abstract protected function fetchTheaters();
 }
-
 
 abstract class MoviesFetcher {
     protected $movieList;
-    public function __construct(Theater $theater, $date) {
+    public function __construct(Theater $theater = null, DateTime $date = null) {
         $list = new MovieList();
         $list->date = $date;
         $list->theater = $theater;
         $this->movieList = $list;
     }
 
+    public function movieList() {
+        if (empty($this->movieList->movies)) {
+            $this->movieList->movies = $this->fetchTheaterMovies();
+        }
+
+        return $this->movieList;
+    }
+
     /**
      * Fetch a list of movies infomation that showing in theater in that date
      */
-    abstract public abstract fetchTheaterMovies();
+    abstract protected function fetchTheaterMovies();
 }
--- src/services/google/fetcher.php
+++ src/services/google/fetcher.php
@@ -1,10 +1,143 @@
 <?php
+require_once 'src/services/fetcher.php';
+require_once 'src/models/matcher.php';
 
-class GoogleTheatersFetcher {
+class GoogleMovie {
+    const URL = 'http://www.google.com/movies';
 
+    public static function theaterListContentURL($zipcode) {
+        $url = self::URL;
+        return "$url?near=$zipcode";
+    }
+
+    public static function movieListContentURL($tid, DateTime $date) {
+        $url = self::URL;
+        if (!isset($date)) $date = new DateTime();
+
+        //Google using day diff to determine showtime date and only give small date span
+        $diff = $date->diff(new DateTime());
+        $day = $diff->d;
+
+        return "$url?tid=$tid&date=$day";
+    }
+}
+
+class GoogleTheatersFetcher extends TheatersFetcher {
+
+    public function __construct($zipcode) {
+        parent::__construct($zipcode);
+    }
+
+    public function fetchTheaters() {
+
+    }
+}
+
+class GoogleMoviesFetcher extends MoviesFetcher  {
+    // protected $movieList;    //inherited from MoviesFetcher
+
+    private $contents;
+
+    public function __construct(Theater $theater, DateTime $date) {
+        parent::__construct($theater, $date);
+        $this->initContents();
+    }
+
+    private function initContents() {
+        $this->contents =array();
+
+        $tid = $this->movieList->theater->tid;
+        $date = $this->movieList->date;
+        $url = GoogleMovie::movieListContentURL($tid, $date);
+        $con = file_get_contents($url);
+
+        if ($con === false) {
+            //TODO: log error here
+            return;
+        }
+        static $separator = '<div class=movie>';
+
+        $arr = explode($separator, $con);
+        if (array_shift($arr) === null) {
+            //TODO: log error here
+            return;
+        }
+
+        $this->contents = $arr;
+    }
+
+    public function fetchTheaterMovies() {
+        $movies = array();
+        foreach ($this->contents as $content) {
+            $movie = $this->fetchMovie($content);
+            if ($movie == null) continue;
+            $movies[] = $movie;
+        }
+        return $movies;
+    }
+
+    protected function fetchMovie($content) {
+        $movie = new Movie();
+        $matcher = new StringMatcher($content);
+        $patternList = self::movieMatchingPatternList();
+
+        $matcher->execute($patternList, $movie);
+
+        $showtimePattern = '(\d+):(\d+)(am|pm)?&';
+        $matches = $matcher->match($showtimePattern, true);
+        if (empty($matches)) {
+            return null;
+        }
+
+        $count = count($matches);
+        $isPM = true;
+        $showtimes = array();
+        for ($i=$count-1; $i >= 0 ; $i--) { 
+            $showtime = $matches[$i];
+            $h = $showtime[1];
+            $m = $showtime[2];
+            if(!empty($showtime[3])) $isPM = ($showtime[3] == 'pm');
+
+            if ($h < 12 && $isPM) $h += 12;
+
+            $showtimes[] = "$h:$m";
+        }
+        
+        $movie->showtimes = array_reverse($showtimes);
+
+        return $movie;
+
+    }
+
+    protected static function movieMatchingPatternList() {
+        static $patternList = null;
+        if ($patternList == null) {
+            $patternList = array(
+                array('name', '>([^><]+?)</a>'),
+                array('mid', 'mid=([a-z0-9]+)'),
+                array('runtime', '(\d+)hr (\d+)min', function($matches){ return $matches[1]*60+$matches[2]; }),
+            );
+        }
+
+        return $patternList;
+    }
 }
 
 
-class GoogleMoviesFetcher {
-    
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
