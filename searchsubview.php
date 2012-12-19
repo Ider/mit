05fe7cd0d43c63c3943e4c88edc6c564887eb82c
Ider <ider.cs@gmail.com>
2012-12-18 22:28:12 -0800

    Search Subview This view will show on main, style the search button and textbox

---
+++ searchsubview.php
@@ -0,0 +1,7 @@
+
+
+<div class="search_container">
+    <form name="searchForm" method='get' action="index.php">
+        <input type="text" name="zipcode" value="" class="search_text" maxlength="5" pattern="\d{5}" required="required" size="10"/><input type="submit" id="searchSubmit" value="Search" class='search_submit'/>
+    </form>
+</div>
