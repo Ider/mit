690396eea13f55660a43436ae38e69944a256b74
Ider <ider.cs@gmail.com>
2012-12-20 17:58:34 -0800

    inclcude javascript file in search view
    include jquery and tippanel js, so that it could be used in any view

--- searchsubview.php
+++ searchsubview.php
@@ -5,3 +5,5 @@
         <input type="text" name="zipcode" value="" class="search_text" maxlength="5" pattern="\d{5}" required="required"  placeholder="zipcode" size="10"/><input type="submit" id="searchSubmit" value="Search" class='search_submit'/>
     </form>
 </div>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="./js/tippanel.js"></script>
