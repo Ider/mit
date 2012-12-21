d5d793f335b6f5688e83b8bde0537e544a785b24
Ider <ider.cs@gmail.com>
2012-12-20 17:57:51 -0800

    auto point to specific position of content
    with calculation, point to correct position, and show the arrow

--- js/tippanel.js
+++ js/tippanel.js
@@ -6,33 +6,55 @@
         return;
     }
 
-    /* Configuration */
+    /* Configuration Default Set*/
     var cf = {
         arrow: {
             position: {head:0, middle:1, rear:2},
             direction: {
-                up: ['bottom', 'left'],
-                down: ['top', 'left'],
-                left: ['right', 'top'],
-                right: ['left', 'top']
-            }
+        /* direction:   [arrow-margin-segment, offset-relative-axi, showat-offset-multiplier] */
+                up:     ['bottom',  'left', +1],
+                down:   ['top',     'left', -1],
+                left:   ['right',   'top',  +1],
+                right:  ['left',    'top',  -1]
+            },
+            pointto: {head:0, middle:1, rear:2}
         },
+        defaultArrow: {
+            position: 'middle',
+            direction: 'down',
+            pointto: 'middle'
+        },
+        formatConfig: function(config, sets, defaults){
+            for (var prop in defaults) {
+                if (!config[prop] || !sets[prop].hasOwnProperty(config[prop])) {
+                    config[prop] = defaults[prop];
+                }
+            }
+
+            return config;
+        },
+
         formatArrowConfig: function(config) {
             var arrow = cf.arrow;
-            if (!config.position || !arrow.position.hasOwnProperty(config.position)) {
-                config.position = 'middle';
+            config = cf.formatConfig(config, cf.arrow, cf.defaultArrow);
+
+            config.size = config.size? config.size : 10;
+            config.border = 0;
+            return config;
+        },
+
+        formatAminate: function(animate) {
+            if (!animate ||!animate.show ||!animate.hide) {
+                return {show:'show', hide:'hide'};
             }
-            if (!config.direction || !arrow.direction.hasOwnProperty(config.direction)) {
-                config.direction = 'down';
-            }
-            if (!config.size) {
-                config.size = 10;
-            }
+
+            return animate;
         }
     };
+
     TipPanel.prototype = {
         genArrow: function(arrowConfig) {
-            if (!arrowConfig) return;
+            if (!arrowConfig) arrowConfig= {};
 
             cf.formatArrowConfig(arrowConfig);
             this.arrow = arrowConfig;
@@ -65,6 +87,7 @@
             if (arrowConfig.noborder || borderwidth <=0) return;
             ++borderwidth;//arrow border is tilted, make 1px giger to make nicer
             size += borderwidth;
+            this.arrow.border = borderwidth;
 
             css['border-width'] = size;
             css['z-index'] = 7;
@@ -75,38 +98,78 @@
             $('<div></div>').css(css).appendTo(this.panel);
         },
 
-        genBottomArrow: function() {
-            this.genArrow({position:'rear', direction:'up', size: 10});
-            this.genArrow({position:'middle', direction:'down', size: 10});
+        show: function(offset) {
+            if (offset) {
+                this.panel.css(offset);
+            }
+            this.panel.stop(true);
+            this.panel.hide();
+            this.panel[this.animate.show]();
         },
-
-        showAt: function(offset) {
-            this.panel.offset(offset);
-        },
-
-        showAbove: function(obj) {
-            var offset = $(obj).offset();
-            offset.top -= this.panel.outerHeight()+this.arrow.size;
-            this.showAt(offset);
-
-        },
-        showUnder: function(obj) {
+        showAt: function(obj) {
             obj = $(obj);
             var offset = obj.offset();
-            offset.top += obj.outerHeight()+this.arrow.size;
-            this.showAt(offset);
+            if (!offset) return;
 
+            var pto = cf.arrow.pointto[this.arrow.pointto],
+                pos = cf.arrow.position[this.arrow.position],
+                dir = cf.arrow.direction[this.arrow.direction],
+                axi = dir[1], mul = dir[2],
+                arw = this.arrow.size+this.arrow.border;
+            
+            var piv, //pivot: for arrow offset
+                abs, //aboslute offset: offset on content
+                rel; //relative offset: offset on arrow and pointer
+            if (axi == 'top') {
+                piv = 'left'; abs = 'outerWidth'; rel = 'outerHeight';
+            } else {
+                piv = 'top';  abs = 'outerHeight'; rel = 'outerWidth';
+            }
+
+            var c = obj[abs](), //content
+                p = this.panel[abs](); //panel
+            offset[piv] += (c-p)/2 + mul*((c+p)/2+arw);
+
+            c = obj[rel](), p = this.panel[rel]();
+            offset[axi] += (pto*c-pos*p)/2;
+
+            this.show(offset);
+        },
+        hide: function(delay) {
+            delay = parseInt(delay, 10);
+            delay = isNaN(delay)? 0: delay;
+            this.panel.delay(delay)[this.animate.hide]();
         }
+
+        // showAbove: function(obj) {
+        //     var offset = $(obj).offset();
+        //     offset.top -= this.panel.outerHeight()+this.arrow.size;
+        //     this.show(offset);
+
+        // },
+
+        // showUnder: function(obj) {
+        //     obj = $(obj);
+        //     var offset = obj.offset();
+        //     offset.top += obj.outerHeight()+this.arrow.size;
+        //     this.show(offset);
+
+        // },
+
+        // showBefore: function(obj) {
+
+        // },
+
+        // showAfter: function(obj) {
+
+        // }
     };
 
     function TipPanel(panel, config) {
         this.panel = panel;
-        this.genBottomArrow();
-        // this.genTopArrow();
-        // this.genLeftArrow();
-        this.arrow = {
-            size:13
-        };
+        this.panel.css('position', 'absolute');
+        this.genArrow(config.arrow);
+        this.animate = cf.formatAminate(config.animate);
     }
 
     
@@ -116,6 +179,9 @@
         panel = $(panel);
         if (panel.length <= 0) {
             panel = $('<div></div>').appendTo($('body'));
+        }
+        if (!config) {
+            config = {};
         }
 
         return new TipPanel(panel, config);
