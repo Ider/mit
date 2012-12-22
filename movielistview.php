4cfebd59a2192aa446be481c29def3a6075485b0
Ider <ider.cs@gmail.com>
2012-12-21 18:50:01 -0800

    port show tip info and add movie to wish
    the code should be rewrite to be neat, now just move code to
    movielistview file

--- movielistview.php
+++ movielistview.php
@@ -43,40 +43,127 @@ $displayer->generate();
 $displayer->show();
 
 ?>
+
+<div class="footer_placeholder"> 
+</div>
+<div id="wishlist_outter_container" class="wishlist_outter_container">
+    <div class="wish_zone">
+        <div id="wishlist_container">
+            <div id="wish_movie_container"></div>
+        </div>
+    </div>
+</div>
 <div id="movie_info_panel" class="movie_info_panel">
     <div id="movie_info_title"></div>
     <div id="movie_info_showtime"></div>
 </div>
-
 <script type="text/javascript">
 (function($) {
     $('#none_script_style').remove();
+    $('#wishlist_outter_container').show();
 
     var config = {  arrow:{position:'middle', direction:'down', pointto:'middle', size: 10},
                     animate: {show: 'show', hide: 'hide'},
         },
-        panel = $.tip('#movie_info_panel', config);
-        panel.title = $('#movie_info_title');
-        panel.showtime = $('#movie_info_showtime');
-        panel.setTitle = function(title, showtime) {
-            this.title.text(title);
-            this.showtime.text(showtime);
+        panel = $.tip('#movie_info_panel', config),
+        ext = {
+                lastPointTo: null,
+                title: $('#movie_info_title'),
+                showtime: $('#movie_info_showtime'),
+                setTitle: function(title, showtime) {
+                    this.title.text(title);
+                    this.showtime.text(showtime);
+                },
+                showAt: function(obj) {
+                    if (this.lastPointTo == obj) return;
+                    this.dismiss();
+                    this.lastPointTo = obj;
+                    
+                    var showtime = $(obj);//.css('z-index', 99);
+                    
+                    this.setTitle(showtime.parent().data('moviename'), 
+                                            showtime.data('showtime'));
+                    this.pointTo(showtime);
+                    this.fadeIn();
+                },
+                dismiss: function() {
+                    // $(this.lastPointTo).css('z-index', '');
+                    this.lastPointTo = null;
+                    this.hide();
+                }
         };
+        $.extend(panel, ext);
 
     var lastHoveredShowtime = null;
     $('.movie_showtime').on('mouseover', function() {
-        lastHoveredShowtime = this;
-        var showtime = $(this).css('z-index', 99);
+        panel.showAt(this);
+    });
+    // .on('mouseout', function() {
+    //     $(lastHoveredShowtime).css('z-index', '');
+    //     panel.hide();
+    // });
 
-        panel.setTitle(showtime.parent().data('moviename'), showtime.data('showtime'));
-        panel.showAt(showtime);
-    })
-    .on('mouseout', function() {
-        $(lastHoveredShowtime).css('z-index', '');
-        panel.hide();
+    $('body').on('click', function() {
+        panel.dismiss();
     });
 
 
+
+//////////following code need refactory////
+
+
+
+
+    var movieShotimes = $('.movie_showtime').css('cursor', 'pointer');
+
+
+    var wishlist = {}; 
+    movieShotimes.on('click', function() {
+
+        var showtime = $(this);
+            movieName = showtime.parent().attr('title'),
+            identifier = showtime.data('showtime') + ' - '+ movieName;
+        
+        // console.log(identifier);
+        if (wishlist.hasOwnProperty(identifier)) {
+            wishlist[identifier].toggle();
+            return;
+        }
+
+        var wishshowtime = showtime.clone();
+        wishMovieContainer.append(wishshowtime);
+        wishshowtime.css({top: 0});
+        wishshowtime.data('moviename', movieName);
+
+        wishlist[identifier] = wishshowtime;
+    });
+
+
+var wishlistContainer = $('#wishlist_container');
+    var style = $('.movie_list_inner_container').attr('style');
+    wishlistContainer.attr('style', style);
+
+    var wishMovieContainer = $('#wish_movie_container');
+    style = $('.showtime_container').attr('style');
+    wishMovieContainer.attr('style', style);
+
+    wishMovieContainer.css({
+        height: '16px',
+        top: '24px',
+    });
+    // titles.slideUp(function() {
+    //             movieShowtimeContainers.css('border', 'none');
+    //             innerContainer.css('border', '1px dashed black')
+    //         });
+    wishMovieContainer.on('click', '.movie_showtime', function() {
+        //move it to top
+        var showtime = $(this);
+        showtime.parent().append(showtime);
+    });
+
+    wishMovieContainer.on('mouseover', '.movie_showtime', function() {
+        panel.showAt(this);
+    });
 
 })(jQuery);
 </script>
